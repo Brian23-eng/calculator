3a4b226a7735856918cf22eeabd7fb38e029c52e
brian23-eng <b.odhiambo.bo@gmail.com>
2019-08-15 11:09:13 +0300

    add calculation syntax to js file

---
+++ Desktop/test/Calculator/script.js
@@ -0,0 +1,9 @@
+var subtract = function(number1, number2){
+ return number1 - number2
+ }
+
+var number1 = parseInt(prompt("Enter first number"));
+var number2 = parseInt(prompt("Enter second number"))
+var result =subtract(number1,number2)
+
+alert(result)
